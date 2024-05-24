d441915ab1d9010d1c272e212020e40f6b0d967f
Hafidz Al Asqalani <60850212+hafidzalasqalani@users.noreply.github.com>
2024-05-24 17:26:06 +0700

    ubah login
    Signed-off-by: Hafidz Al Asqalani <60850212+hafidzalasqalani@users.noreply.github.com>

--- application/views/form/v_login.php
+++ application/views/form/v_login.php
@@ -23,7 +23,7 @@
         <div class="brand-logo"></div>
         <div class="brand-title">LOGIN</div>
         <form method="post" action="<?php echo base_url(); ?>form/login/login">
-            <label>Username Kamu : </label>
+            <label>Username Kamu ya: </label>
             <input type="text" name="username">
             <br>
             <label>Password : </label>
